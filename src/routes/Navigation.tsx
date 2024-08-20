71db0584012d761e89666e50dfe563f8456a010a
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2024-08-20 08:46:25 +0200

    chore: Project init

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from "react-router-dom"
+import logo from "../logo.svg"
+
+export const Navigation = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="logo" />
+            <ul>
+              <li>
+                <NavLink
+                  to="/home"
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  to="/about"
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  to="/users"
+                >
+                  Users
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/home" element={<h1>Home Page</h1>} />
+            <Route path="/about" element={<h1>About Page</h1>} />
+            <Route path="/users" element={<h1>Users Page</h1>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
